20690cdc2cd8f94c8fc9f236eae72231036af4bf
Galanta <92652226+GalantaTech@users.noreply.github.com>
2025-03-27 17:35:51 -0300

    Adicionar configuração Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,46 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     main: './src/public/js/main.ts',
-    particles: './src/public/js/particles-config.ts'
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
-    ],
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.ts', '.js']
   },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'public/js'),
-  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: 'src/public', 
+          to: '.', 
+          globOptions: {
+            ignore: ['**/*.ts']
+          }
+        }
+      ]
+    })
+  ]
 };
