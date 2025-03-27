4721a76c85b8ae7e14e3be12b642ff6fc1921222
Galanta <92652226+GalantaTech@users.noreply.github.com>
2025-03-27 17:35:58 -0300

    Adicionar arquivo principal do servidor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import path from 'path';
 
-// Inicialização do app Express
 const app = express();
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 3000;
 
-// Configuração de middleware para servir arquivos estáticos
+// Configurar arquivos estáticos
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Rota principal
@@ -13,7 +12,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Inicialização do servidor
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
